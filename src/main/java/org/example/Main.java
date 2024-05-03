4e88f99ddcebe895ab8e58d6b5c0ec884d91edf8
tuhucon <minhtuan93@yahoo.com>
2024-05-03 11:15:27 +0700

    use GrapDSL.create(sink,(builder,out)... when we want to get output from Graph. use GrapDSL,create(builder... when we dont want to get output from Graph.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,61 +1,58 @@
 package org.example;
 
-import akka.Done;
 import akka.NotUsed;
 import akka.actor.typed.ActorSystem;
-import akka.actor.typed.SupervisorStrategy;
 import akka.actor.typed.javadsl.Behaviors;
-import akka.cluster.ClusterEvent;
-import akka.cluster.sharding.typed.javadsl.ClusterSharding;
-import akka.cluster.sharding.typed.javadsl.Entity;
-import akka.cluster.sharding.typed.javadsl.EntityTypeKey;
-import akka.cluster.sharding.typed.javadsl.ShardedDaemonProcess;
-import akka.cluster.typed.Cluster;
-import akka.cluster.typed.ClusterSingleton;
-import akka.cluster.typed.ClusterSingletonSettings;
-import akka.cluster.typed.SingletonActor;
-import akka.cluster.typed.Subscribe;
-import akka.management.javadsl.AkkaManagement;
-import akka.persistence.jdbc.state.DurableStateQueries;
-import akka.persistence.jdbc.state.javadsl.JdbcDurableStateStore;
-import akka.persistence.jdbc.testkit.javadsl.SchemaUtils;
-import akka.persistence.state.javadsl.DurableStateUpdateStore;
-import akka.persistence.typed.PersistenceId;
-import akka.stream.ActorMaterializer$;
-import akka.stream.ActorMaterializerHelper;
-import akka.stream.Materializer;
+import akka.stream.ClosedShape;
+import akka.stream.FlowShape;
+import akka.stream.UniformFanInShape;
+import akka.stream.UniformFanOutShape;
 import akka.stream.javadsl.Broadcast;
 import akka.stream.javadsl.Flow;
-import akka.stream.javadsl.Keep;
+import akka.stream.javadsl.GraphDSL;
+import akka.stream.javadsl.Merge;
 import akka.stream.javadsl.RunnableGraph;
 import akka.stream.javadsl.Sink;
 import akka.stream.javadsl.Source;
-import org.example.cluster.DirectorBehavior;
-import org.example.cluster.WorkerBehavior;
-import org.example.cluster.sharding.DurableUser;
-import org.example.cluster.sharding.EventSourceUser;
-import org.example.cluster.sharding.User;
-import scala.Int;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
+
+    public static RunnableGraph<CompletionStage<Integer>> createOneSource2FlowGraph() {
+        Source<Integer, NotUsed> source = Source.range(1, 100);
+        Flow<Integer, Integer, NotUsed> flow1 = Flow.of(Integer.class).map(i -> i * 2);
+        Flow<Integer, Integer, NotUsed> flow2 = Flow.of(Integer.class).map(i -> i * 3);
+        Sink<Integer, CompletionStage<Integer>> sink = Sink.fold(0, Integer::sum);
+
+        //use GrapDSL.create(sink,(builder,out)... when we want to get output from Graph.
+        //use GrapDSL,create(builder... when we dont want to get output from Graph.
+        RunnableGraph<CompletionStage<Integer>> graph = RunnableGraph.fromGraph(GraphDSL.create(sink, (builder, out) -> {
+            //Create Junctions
+            UniformFanOutShape<Integer, Integer> broadCast = builder.add(Broadcast.create(2));
+            UniformFanInShape<Integer, Integer> merge = builder.add(Merge.create(2));
+
+            builder.from(builder.add(source).out())
+                    .viaFanOut(broadCast)
+                    .via(builder.add(flow1))
+                    .viaFanIn(merge)
+                    .to(out);
+
+            builder.from(broadCast)
+                    .via(builder.add(flow2))
+                    .viaFanIn(merge);
+
+
+            return ClosedShape.getInstance();
+        }));
+        return graph;
+    }
 
     public static void main(String[] args) throws InterruptedException {
         ActorSystem<Void> actorSystem = ActorSystem.create(Behaviors.empty(), "test");
 
-        Source<Integer, NotUsed> source = Source.range(1, 100);
-
-        Sink<Integer, CompletionStage<Integer>> sink = Sink.fold(0, Integer::sum);
-
-        RunnableGraph<CompletionStage<Integer>> runnableGraph = source.toMat(sink, Keep.right());
-
-        CompletionStage<Integer> result = runnableGraph.run(actorSystem);
-
-
-
+        CompletionStage<Integer> result = createOneSource2FlowGraph().run(actorSystem);
 
         result.thenAccept(sum -> {
             System.out.println("SUM = " + sum);
