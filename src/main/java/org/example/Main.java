156c67c4d1f96336c55c90f13b8ddf38deefefb3
tuhucon <minhtuan93@yahoo.com>
2024-05-06 14:42:50 +0700

    use a BidiFlow

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,19 @@
 package org.example;
 
+import akka.Done;
 import akka.NotUsed;
 import akka.actor.typed.ActorSystem;
 import akka.actor.typed.javadsl.Behaviors;
+import akka.japi.Pair;
 import akka.stream.ClosedShape;
 import akka.stream.FlowShape;
 import akka.stream.UniformFanInShape;
 import akka.stream.UniformFanOutShape;
+import akka.stream.javadsl.BidiFlow;
 import akka.stream.javadsl.Broadcast;
 import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.GraphDSL;
+import akka.stream.javadsl.Keep;
 import akka.stream.javadsl.Merge;
 import akka.stream.javadsl.RunnableGraph;
 import akka.stream.javadsl.Sink;
@@ -18,7 +22,9 @@ import jnr.ffi.annotations.In;
 import scala.collection.generic.BitOperations;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 
@@ -74,17 +80,35 @@ public class Main {
         }));
     }
 
+    public static RunnableGraph<CompletionStage<Integer>> createOneRandomSourceGraph() {
+        Source<Integer, NotUsed> source = Source.unfold(0, s -> {
+            Integer random = ThreadLocalRandom.current().nextInt(10);
+            if (random > 8 && s > 0) {
+                return Optional.empty();
+            } else {
+                return Optional.of(new Pair<Integer, Integer>(s + random, random));
+            }
+        });
+
+        Sink<Integer, CompletionStage<Integer>> sink = Sink.fold(0, Integer::max);
+        return source.toMat(sink, Keep.right());
+    }
+
+    public static RunnableGraph<NotUsed> createBidiFlowGraph() {
+        Source<Integer, NotUsed> source = Source.range(1, 3);
+        var bidiFlow = BidiFlow.fromFlows(Flow.of(Integer.class).map(i -> i * 3), Flow.of(Integer.class).map(i -> i * 10));
+        Sink<Integer, CompletionStage<Done>> sink = Sink.foreach(i -> {
+            System.out.println(i + "");
+        });
+        return source.via(bidiFlow.join(Flow.of(Integer.class)))
+                .to(sink);
+    }
+
     public static void main(String[] args) throws InterruptedException {
         ActorSystem<Void> actorSystem = ActorSystem.create(Behaviors.empty(), "test");
 
-        RunnableGraph<CompletionStage<Integer>> graph = createThreeSourceOneSinkGraph();
-        var result = graph.run(actorSystem);
-
-        result.thenAccept(x -> {
-            System.out.println("max = " + x);
-        });
-
-
+        RunnableGraph<NotUsed> randomSourceGraph = createBidiFlowGraph();
+        randomSourceGraph.run(actorSystem);
 
         try {
             System.in.read();
